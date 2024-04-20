a20ca5e120ff3d45877b6e2c6aa82d2e322a8472
Keitumetse Kgatlhanye <keitumetselovely@gmail.com>
2024-04-20 05:11:06 -0700

    task 1

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,11 @@
+package { 'python3-pip':
+  ensure => installed,
+}
+
+exec { 'install_flask':
+  command     => '/usr/bin/pip3 install Flask==2.1.0',
+  path        => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+  environment => ['HOME=/root'],
+  unless      => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
+}
+
